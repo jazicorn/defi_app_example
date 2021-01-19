31f4f4c7c96f442e81e634a2dce0947029d45638
Jasmine Anderson <jazz2900@gmail.com>
2021-01-19 03:15:53 -0500

    transfer tokens between accounts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -14,4 +14,10 @@ module.exports = async function(deployer, network, accounts) {
   // Deploy TokenFarm
   await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
   const tokenFarm = await TokenFarm.deployed()
+
+  // Transfer all tokens to TokenFarm (1 million)
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+  // Transfer 100 Mock DAI to investor
+  await daiToken.transfer(accounts[1], '1000000000000000000000000')
 };

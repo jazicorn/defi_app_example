0ad5eda01d7fcefab71732ed06e549062583477f
Jasmine Anderson <jazz2900@gmail.com>
2021-01-23 12:58:19 -0500

    can now deposit tokens to stakers

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -7,9 +7,37 @@ contract TokenFarm {
     string public name = "Dapp Token Farm";
     DappToken public dappToken;
     DaiToken public daiToken;
+
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
     
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
     }
+
+    //Stake Tokens (Deposit)
+    function stakeTokens(uint _amount) public {
+        //Code goes inside here
+
+        // Transfer Mock Dai to this contract for staking
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        // Update stakign Balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        // Add user to stakers array *only* if they haven't staked already
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        isStaked[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+
+    // Unstake Tokens (Withdraw)
+
+    // Issue Tokens
 }

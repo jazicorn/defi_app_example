b5e384a93dce70a06b11bc8210a4b471b38240a2
Jasmine Anderson <jazz2900@gmail.com>
2021-01-23 11:42:06 -0500

    updated test to load contracts and transfer coin

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -7,10 +7,19 @@ require('chai')
     .should()
     
 contract('TokenFarm', (accounts) => {
-    // Write tests here...
+    let daiToken, dappToken, tokenFarm
+
+    before(async () => {
+        // load Contracts
+        daiToken = await DaiToken.new()
+        dappToken = await DappToken.new()
+        tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
+
+        await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+    })
+
     describe('Mock Dai deployment', async () => {
         it('has a name', async () => {
-            let daiToken = await DaiToken.new()
             const name = await daiToken.name()
             assert.equal(name, 'Mock DAI Token')
         })

11949850d85ce28474863aedf714603d2554f67f
Jasmine Anderson <jazz2900@gmail.com>
2021-01-23 11:19:34 -0500

    created test for TokenFarm

---
+++ test/TokenFarm.test.js
@@ -0,0 +1,18 @@
+const DaiToken = artifacts.require('DaiToken')
+const DappToken = artifacts.require('DappToken')
+const TokenFarm = artifacts.require('TokenFarm')
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+    
+contract('TokenFarm', (accounts) => {
+    // Write tests here...
+    describe('Mock Dai deployment', async () => {
+        it('has a name', async () => {
+            let daiToken = await DaiToken.new()
+            const name = await daiToken.name()
+            assert.equal(name, 'Mock DAI Token')
+        })
+    })
+})

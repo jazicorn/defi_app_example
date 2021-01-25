043e86ce63c90834b0e594e485894e4edbb3c1aa
Jasmine Anderson <jazz2900@gmail.com>
2021-01-24 19:06:12 -0500

    added to issue token testing

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -85,6 +85,9 @@ contract('TokenFarm', ([owner, investor]) => {
             // Check balances after issuance
             result = await dappToken.balanceOf(investor)
             assert.equal(result.toString(), tokens('100', 'investor DApp Token wallet balance correct after issuance'))
+
+            // Ensure that only owner can issue tokens
+            await tokenFarm.issueToken({ from: investor }).should.be.rejected;
         })
     })
 })
